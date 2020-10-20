732af1d379ad8c258ad72e6c52735a2999d6f62b
Marcelo Freitas | mfreitas365 <55969481+mfreitas365@users.noreply.github.com>
2020-10-20 09:14:12 -0300

    Add files via upload

---
+++ MS-203-scripts.ps1
@@ -0,0 +1,13 @@
+﻿# Study MS-203 - M365 Messaging
+#Commands
+
+
+Get-TransportPipeline | FL Event,TransportAgents
+
+Get-/Set-TransportService
+
+Get-/Set-MailboxTransportService
+
+Get-/Set-FrontendTransportService
+
+
