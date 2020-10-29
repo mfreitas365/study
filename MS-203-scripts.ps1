a194fdfdaa5d616e8faa170fcd8d4e56c79dcbb0
Marcelo Freitas | mfreitas365 <55969481+mfreitas365@users.noreply.github.com>
2020-10-28 23:26:53 -0300

    Update MS-203-scripts.ps1

--- MS-203-scripts.ps1
+++ MS-203-scripts.ps1
@@ -10,4 +10,24 @@ Get-/Set-MailboxTransportService
 
 Get-/Set-FrontendTransportService
 
+#Transport Service
+Get-ReceiveConnector
+Get-SendConnector 
+
+
+#Connect to Exchange Online
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -DisableNameChecking
+Get-Mailbox
+Get-EXOMailbox
+Get-EXOCasMailbox
+# checkout https://aka.ms/exops-docs
+# for using Exchange Online V2 Module which uses Modern Authentication.
+
+
+#Config DKIM
+New-DkimSigningConfig -DomainName <domain> -Enabled $false
+Get-DkimSigningConfig -Identity demosmfreitas365security.online | Format-List Selector1CNAME, Selector2CNAME
+#end
 
