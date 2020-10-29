a89a1b422d9e67b64da8c69f666b05427546753d
Marcelo Freitas | mfreitas365 <55969481+mfreitas365@users.noreply.github.com>
2020-10-29 07:50:01 -0300

    Update MS-203-scripts.ps1
    Add another option to Connection to Exchange Online PowerShell.

--- MS-203-scripts.ps1
+++ MS-203-scripts.ps1
@@ -3,11 +3,8 @@
 
 
 Get-TransportPipeline | FL Event,TransportAgents
-
 Get-/Set-TransportService
-
 Get-/Set-MailboxTransportService
-
 Get-/Set-FrontendTransportService
 
 #Transport Service
@@ -16,6 +13,7 @@ Get-SendConnector
 
 
 #Connect to Exchange Online
+{
 $UserCredential = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 Import-PSSession $Session -DisableNameChecking
@@ -24,10 +22,19 @@ Get-EXOMailbox
 Get-EXOCasMailbox
 # checkout https://aka.ms/exops-docs
 # for using Exchange Online V2 Module which uses Modern Authentication.
+}
 
+#Connect to Exchange Online PowerShell
+{
+Install-Module -Name ExchangeOnlineManagement -RequiredVersion 2.0.3
+#Connect with user MFA
+Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnline -UserPrincipalName marcelo.freitas@mfreitas365.net -ShowProgress $true
+Disconnect-ExchangeOnline
+}
 
 #Config DKIM
-New-DkimSigningConfig -DomainName <domain> -Enabled $false
+New-DkimSigningConfig -DomainName demosmfreitas365security.online -Enabled $false
 Get-DkimSigningConfig -Identity demosmfreitas365security.online | Format-List Selector1CNAME, Selector2CNAME
 #end
 
