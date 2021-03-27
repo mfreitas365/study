0cb8e215027607e28ff43f0a85bf1f48e56819e2
Marcelo Fernandes de Freitas <55969481+mfreitas365@users.noreply.github.com>
2021-03-27 13:29:26 -0300

    Update MS-101.ps1

--- MS-101.ps1
+++ MS-101.ps1
@@ -1,10 +1,17 @@
 # MS-101
 
-Import-Module MSOnline
+# check version of powershell module
+$PSVersiontable
 
+# mode unrestricted to run commands
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
+
+
+Install-Module -Name AzureAD
 
 $Credential = Get-Credential
 Connect-AzureAD -Credential $Credential
+
 
 # Create a password object
 $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
@@ -13,7 +20,9 @@ $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordPro
 $PasswordProfile.Password = "emc2@4ADM21"
 
 # Create the new user
-New-AzureADUser -AccountEnabled $True -DisplayName "Abby Brown" -PasswordProfile $PasswordProfile -MailNickName "AbbyB" -UserPrincipalName "AbbyB@contoso.com"
+New-AzureADUser -AccountEnabled $True -DisplayName "Abby Brown" -PasswordProfile $PasswordProfile -MailNickName "AbbyB" -UserPrincipalName "AbbyB@contosomfreitas365.onmicrosoft.com"
+
+
 
 
 
