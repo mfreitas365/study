7e3ea4871b6fb0c82353e5bade54afee9868ed88
Marcelo Fernandes de Freitas <55969481+mfreitas365@users.noreply.github.com>
2021-04-07 06:45:45 -0300

    Study
    Study MS-101

--- MS-101.ps1
+++ MS-101.ps1
@@ -1,6 +1,8 @@
 # MS-101
 
+#---------------------------------------
 # check version of powershell module
+{
 $PSVersiontable
 
 # mode unrestricted to run commands
@@ -21,9 +23,36 @@ $PasswordProfile.Password = "emc2@4ADM21"
 
 # Create the new user
 New-AzureADUser -AccountEnabled $True -DisplayName "Abby Brown" -PasswordProfile $PasswordProfile -MailNickName "AbbyB" -UserPrincipalName "AbbyB@contosomfreitas365.onmicrosoft.com"
+New-AzureADUser -AccountEnabled $True -DisplayName "Albert Einstein" -PasswordProfile $PasswordProfile -MailNickName "Albert" -UserPrincipalName "AlbertE@contosomfreitas365.onmicrosoft.com"
+
+}
 
 
 
+#---------------------------------------
+# You can bulk create member users and guests accounts. The following example shows how to bulk invite guest users.
+{                  
+$invitations = import-csv c:\bulkinvite\invitations.csv
+
+$messageInfo = New-Object Microsoft.Open.MSGraph.Model.InvitedUserMessageInfo
+
+$messageInfo.customizedMessageBody = "Hello. You are invited to the Contoso organization."
+
+foreach ($email in $invitations)
+   {New-AzureADMSInvitation `
+      -InvitedUserEmailAddress $email.InvitedUserEmailAddress `
+      -InvitedUserDisplayName $email.Name `
+      -InviteRedirectUrl https://myapps.microsoft.com `
+      -InvitedUserMessageInfo $messageInfo `
+      -SendInvitationMessage $true
+   }
+
+# CSV File format
+# InvitedUserEmailAddress, Name
+
+}
+
+#---------------------------------------
 
 
 
