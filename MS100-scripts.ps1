75c860d484d460913c0db3dda8f2a5a720db685c
Marcelo Freitas | mfreitas365 <55969481+mfreitas365@users.noreply.github.com>
2021-04-18 23:52:13 -0300

    Update MS100-scripts.ps1

--- MS100-scripts.ps1
+++ MS100-scripts.ps1
@@ -30,8 +30,15 @@ Get-MsolRoleMember –RoleObjectId $role.ObjectId
 #Identify users who have registered for MFA 
 Get-MsolUser -All | Where-Object {$_.StrongAuthenticationMethods -ne $null -and $_.BlockCredential -eq $False} | Select-Object -Property UserPrincipalName
 
+#Identify users who have not registered for MFA 
+Get-MsolUser -All | Where-Object {$_.StrongAuthenticationMethods.Count -eq 0 -and $_.BlockCredential -eq $False} | Select-Object -Property UserPrincipalName
 
 
 
+Get-MsolUser -All | Select-Object @{N='UserPrincipalName';E={$_.UserPrincipalName}},
+
+@{N='MFA Status';E={if ($_.StrongAuthenticationRequirements.State){$_.StrongAuthenticationRequirements.State} else {"Disabled"}}},
+
+@{N='MFA Methods';E={$_.StrongAuthenticationMethods.methodtype}} | Export-Csv -Path c:\temp\MFA_Report.csv -NoTypeInformation
 
 
